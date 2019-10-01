b3fa37600e851911fc0e6606f09c311258ef9e8d
Tom White <eugene.white@gmail.com>
2019-10-01 18:36:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @user = User.find(session[:user_id])
-    if @user 
-      erb :account
-    end
-    erb :error
+    erb :account
   end
 
   get '/logout' do
